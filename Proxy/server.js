fdf618b75347bcb58bc4d98a3c74ca13177aacce
Christopher Doornbos <c3doornb@gmail.com>
2019-05-02 14:53:34 -0700

    update for new database

--- Proxy/server.js
+++ Proxy/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+require('newrelic');
 const cors = require('cors');
 const axios = require('axios');
 
@@ -8,29 +9,29 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, './public')));
+app.use('/:stockId', express.static(path.join(__dirname, './public')));
 
 const chartReq = axios.create({
-    baseURL: 'http://ec2-13-57-177-212.us-west-1.compute.amazonaws.com:2468/'
-  });
-  
-  app.get('/api/:stockId', (req, res) => {
-    chartReq.get(`api/${req.params.stockId}`)
-    .then((response) => {
-      res.send(response.data);
-    })
-  })
+  baseURL: 'http://localhost:4000/'
+});
 
-  const buyFormReq = axios.create({
-      baseURL: 'http://ec2-3-84-115-167.compute-1.amazonaws.com:8080/' 
-  });
-
-  app.get('/stocks/:query', (req, res) => {
-      buyFormReq.get(`stocks/${req.params.query}`)
-      .then((response) => {
-          res.send(response.data);
-      })
+app.get('/api/:stockId', (req, res) => {
+  chartReq.get(`api/${req.params.stockId}`)
+  .then((response) => {
+    res.send(response.data);
   })
+})
+
+// const buyFormReq = axios.create({
+//     baseURL: 'http://ec2-3-84-115-167.compute-1.amazonaws.com:8080/' 
+// });
+
+// app.get('/stocks/:query', (req, res) => {
+//     buyFormReq.get(`stocks/${req.params.query}`)
+//     .then((response) => {
+//         res.send(response.data);
+//     })
+// })
 
 app.listen(port, () => {
     console.log('Server is listening on port', port);
